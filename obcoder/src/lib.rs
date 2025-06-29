78e92b05d936e43a3aa582b9d3125cbaad01a3fc
weiminghao <1090498448@qq.com>
2025-06-30 01:17:22 +0800

    Add `ObStream` implementation with frame read/write methods and associated unit test.

--- obcoder/src/lib.rs
+++ obcoder/src/lib.rs
@@ -1,6 +1,53 @@
 mod bindings;
 
+use std::error::Error;
 pub use bindings::*;
 use std::ffi::{CStr, CString};
 
 mod test;
+
+pub fn calc_image_size(width:i32,height:i32,format:AVPixelFormat)  -> usize {
+    unsafe { return calculate_frame_size(width, height, format) };
+}
+
+
+impl ObStream {
+    pub fn new(width:i32,height:i32,src_format:AVPixelFormat, dst_format:AVPixelFormat) -> Self {
+        unsafe {  *create_ob_stream(width, height, 5, src_format,dst_format ) }
+    }
+
+    pub fn destroy(&mut self) {
+        unsafe {
+            destroy_ob_stream(self as *mut ObStream);
+        }
+
+    }
+
+    pub fn write_frame(&mut self, image_data:&mut [u8], image_size:usize) ->Result<(),Box<dyn Error>>{
+        let ret = unsafe {ob_stream_write_frame(self,image_data.as_mut_ptr(),image_size)};
+        if ret == 0 {
+            Ok(())
+        }else{
+            Err("can not write frame data!".into())
+        }
+    }
+
+    ///
+    /// you need to unref and free the AVFrame struct without rustc
+    /// you get this `AVFrame` is ref a cache frame in ObStream,
+    /// you must call `av_frame_unref` to unref the frame and use
+    /// `av_frame_free` to free this struct's memory
+    ///
+    pub fn read_frame(&mut self) ->Result<AVFrame,Box<dyn Error>>{
+        unsafe {
+            let frame = av_frame_alloc();
+            let ret = ob_stream_get_frame(self,frame);
+            if ret == 0 {
+                return Ok(*frame);
+            }
+            else {
+                return Err("can not get frame data!".into());
+            }
+        }
+    }
+}
--- obcoder/src/test.rs
+++ obcoder/src/test.rs
@@ -1,5 +1,5 @@
 use std::ffi::{CStr, CString};
-use crate::{avcodec_find_encoder_by_name,get_encoder};
+use crate::{avcodec_find_encoder_by_name, destroy_ob_stream, get_encoder, AVPixelFormat, AVPixelFormat_AV_PIX_FMT_BGRA, AVPixelFormat_AV_PIX_FMT_YUV420P, ObStream};
 
 #[test]
 fn test() {
@@ -19,4 +19,23 @@ fn test_codec(){
         let codec_name = CStr::from_ptr((*codec).name).to_string_lossy().into_owned();
         println!("{:?}", codec_name);
     }
+}
+
+#[test]
+fn test_ob_stream_new(){
+    #[test]
+    fn test_ob_stream_new(){
+        let mut stream = ObStream::new(1920, 1080, AVPixelFormat_AV_PIX_FMT_BGRA, AVPixelFormat_AV_PIX_FMT_YUV420P);
+        println!("Created stream: {:?}", stream);
+
+        // 检查所有指针
+        println!("ctx: {:?}", stream.ctx);
+        println!("rb: {:?}", stream.rb);
+        println!("tmp_buffer: {:?}", stream.tmp_buffer);
+        println!("tmp_frame: {:?}", stream.tmp_frame);
+        println!("dst_frame: {:?}", stream.dst_frame);
+
+        stream.destroy();
+    }
+
 }
